3ac12b9117a7fbb2a91f568e7570d885cfad0ef2
yy <1667253917@qq.com>
2024-05-16 23:33:58 +0800

    聊天导航页面完成，详细页待更新

---
+++ pages/chat/index.js
@@ -0,0 +1,151 @@
+// pages/chat/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    chating:[
+      // {
+      //   img: "user.avatar",
+      //   url: "/pages/chitchat/index",
+      //   username:"发时刻",
+      //   status:""
+      // },
+      {
+      img: "imagines/新朋友.png",
+      url: "/pages/chitchat/index",
+      username:"新朋友",
+      status:"没有新通知"
+    },
+    {
+      img: "imagines/消息_互动消息.png",
+      url: "/pages/chitchat/index",
+      username:"互动消息",
+      status:"没有新通知"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+    {
+      img: "https://c-ssl.duitang.com/uploads/blog/202207/19/20220719130845_f0846.jpeg",
+      url: "/pages/chitchat/index",
+      username:"吴奕民小狗",
+      status:"在线"
+    },
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
