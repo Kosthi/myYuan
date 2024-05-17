631e2b184780d4cf3bbad7e02ddc0fbe1c963229
yy <1667253917@qq.com>
2024-05-18 02:14:09 +0800

    增加新页面, 实现搜索位置

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,11 +1,47 @@
-// pages/add/add.js
+// pages/friend/friend.js
 Page({
 
     /**
      * 页面的初始数据
      */
-    data: {},
+    data: {
+        tag: [
+            {
+                img: "../../assets/image/friend/地图.png",
+                url: "/pages/locate/locate",
+                username: "你在哪里"
+            },
+            {
+                img: "../../assets/image/friend/更多.png",
+                url: "/pages/chitchat/chitchat",
+                username: "添加标签"
+            },
+            {
+                img: "../../assets/image/friend/公开.png",
+                url: "/pages/chitchat/chitchat",
+                username: "公开·所有人可见"
+            },
+            {
+                img: "../../assets/image/friend/设置.png",
+                url: "/pages/chitchat/chitchat",
+                username: "高级设置"
+            }],
 
+        chooseVideo() {
+            wx.chooseVideo({
+                sourceType: ['album'], // 从相册选择视频
+                maxDuration: 60, // 视频最长拍摄时间，单位秒
+                camera: 'back', // 使用后置摄像头
+                success(res) {
+                    console.log('选择视频成功', res.tempFilePath);
+                    // 在这里可以处理选择视频成功后的逻辑，例如上传视频等
+                },
+                fail(err) {
+                    console.error('选择视频失败', err);
+                }
+            })
+        },
+    },
     /**
      * 生命周期函数--监听页面加载
      */
